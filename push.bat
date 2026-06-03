22aad6230e28681e2193ef9ddf090b7dafc1b3b1
Admin <admin@yuantong.com>
2026-06-03 10:48:19 +0800

    update: 2026/06/03 周三 10:48:19.37

--- push.bat
+++ push.bat
@@ -1,9 +1,6 @@
 @echo off
-chcp 65001 >nul
 cd /d "%~dp0"
 git add -A
 git commit -m "update: %date% %time%"
 git push origin master:main
-echo.
-echo Push completed! Press any key to close...
-pause >nul
+pause
